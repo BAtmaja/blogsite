2c8f69bf4be0140f7d020a500035b750ac8cdfc6
BAtmaja <atmaja.narayan@gmail.com>
2020-09-07 21:51:47 +0530

    Updates

--- src/helpers/index.js
+++
@@ -1,5 +0,0 @@
-export * from './config';
-export * from './history';
-export * from './store';
-export * from './auth-header';
-export * from './fake-backend';
--- src/routes/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-
-export const PrivateRoute = ({ component: Component, ...rest }) => (
-    localStorage.getItem('user')
-    ? <Route path="/" component={Component} />
-    : <Redirect to={{ pathname: '/home' }} />
-)
